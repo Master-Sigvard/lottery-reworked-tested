de61b9e6e6b84b2bda6f8dd682288a3bb20d9497
Ivan <datskoivan19@gmail.com>
2023-03-06 17:04:39 +0200

    start

---
+++ contracts/LotteryReworked.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+abstract contract RandomNumbers is VRFConsumerBaseV2 {
+    
+}
+
+/// @title Lottery
+contract Lottery is Ownable{
+    ///@dev array with addresses of all players
+    address payable[] public players;
+    uint tickets = 4;
+    ///@notice cost of tickets
+    uint cost = 5 ether;
+    uint i;
+
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function random(uint _number) public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, msg.sender))) % _number;
+    }
+
+    function enterLottery() public payable {
+        require(msg.value == cost && tickets > 0);
+        tickets--;
+        ///@dev adding new player to players array
+        players.push(payable(msg.sender));
+    }
+
+    function chooseWinner() external onlyOwner {
+        
+    }
+}
